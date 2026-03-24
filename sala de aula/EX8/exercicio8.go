b8a047994b209e79e93a38b92f4e1158829f6b99
paulomorais-code <paulomorais@discente.ufg.br>
2026-03-23 21:02:41 -0300

    Update exercicio8.go

--- sala de aula/EX8/exercicio8.go
+++ sala de aula/EX8/exercicio8.go
@@ -2,20 +2,20 @@ package main
 
 import "fmt"
 
-func media(nums []int) float64 {
-	if len(nums) == 0 {
-		return 0
-	}
-
-	soma := 0
-	for _, n := range nums {
-		soma += n
-	}
-
-	return float64(soma) / float64(len(nums))
-
-}
 func main() {
-	valores := []int(fmt.Scan())
-	fmt.Println("Media", media(valores))
+	var quantidade int
+	var soma, nota float64
+	soma = 0
+	fmt.Println("Quantas notas serão coletadas?")
+	fmt.Scan(&quantidade)
+
+	for i := 0; i < quantidade; i++ {
+		fmt.Println("Nota:")
+		fmt.Scan(&nota)
+		soma += nota
+	}
+	media := soma / float64(quantidade)
+
+	fmt.Printf("média é = %2.f\n", media)
+
 }
