0a1fe80a4500a4e2c8f74c16d3d1cadce0b38f4d
paulomorais-code <paulomorais@discente.ufg.br>
2026-03-23 21:25:02 -0300

    Update exercicio6.go
    Added validation to check if the input values can form a triangle.

--- sala de aula/EX6/exercicio6.go
+++ sala de aula/EX6/exercicio6.go
@@ -1,18 +1,23 @@
-package main 
+package main
 
 import "fmt"
 
-func main () {
+func main() {
 	var a, b, c int
 
 	fmt.Println("escreva os lados do triangulo:")
-	fmt.Scan(&a, &b,&c)
+	fmt.Scan(&a, &b, &c)
+
+	if a >= (b+c) || b >= (a+c) || c >= (a+b) {
+		fmt.Print("valores inválidos, não é triangulo")
+		return
+	}
 
 	if a == b && b == c && a == c {
-	fmt.Println("o triangulo e equilatero")
-    } else if a == b || b == c || c == a {
-	fmt.Println("o triangulo e isoceles")
-    } else {
-	fmt.Println ("o triangulo é escaleno")
-    }
+		fmt.Println("o triangulo e equilatero")
+	} else if a == b || b == c || c == a {
+		fmt.Println("o triangulo e isoceles")
+	} else {
+		fmt.Println("o triangulo é escaleno")
+	}
 }
